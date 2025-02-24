54972ac811466d696b6e211717f24bce548e4c04
VladimirK <daemon.from.kg@gmail.com>
2025-02-24 14:08:00 +0600

    theme selector

--- src/lib/components/Toolbar.svelte
+++ src/lib/components/Toolbar.svelte
@@ -1,19 +1,38 @@
 <script lang="ts">
+    import DarkIcon from '@/lib/icons/Dark.svelte';
+    import LightIcon from '@/lib/icons/Light.svelte';
+
+    let isDark = true;
+    let themeIcon = LightIcon;
+    const onChangeTheme = () => {
+      isDark = !isDark;
+      window.document.body.classList.toggle('dark', isDark);
+      window.document.body.classList.toggle('light', !isDark);
+      themeIcon = isDark ? LightIcon : DarkIcon;
+    };
+
     const print = () => window.print();
 </script>
 
-<div class="no-print">
-    <button onclick={print}>🖨</button>
+<div class="no-print actions">
+    <button onclick={print}>
+        <span>🖨</span>
+    </button>
+    <button onclick={onChangeTheme}>
+        <svelte:component this={themeIcon} />
+    </button>
 </div>
 
 <style>
-    div {
+    .actions {
       position: fixed;
       top: 120px;
       left: 50%;
       margin-left: 105mm;
       display: flex;
       flex-direction: column;
+      align-content: center;
+      justify-content: center;
       gap: 16px 0;
 
       button {
@@ -27,6 +46,10 @@
         border: 1px solid rgba(255, 255, 255, 0.2);
         border-radius: 4px;
         cursor: pointer;
+
+        :global(svg) {
+          display: block;
+        }
       }
 
       @media (max-width: 992px) {
