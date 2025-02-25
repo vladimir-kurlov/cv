f67c158d69cae042e5149a085e9801c2642eff5c
VladimirK <daemon.from.kg@gmail.com>
2025-02-25 17:00:46 +0600

    add base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tsconfigPaths()],
+  base: '/cv/',
   define: {
     __BUILD_TIME__: Date.now(),
   }
